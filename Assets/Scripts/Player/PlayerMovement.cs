31a69b581494f4e55514531b32ebecd407a4bb7c
reutmgc <reutgaming@gmail.com>
2023-07-22 23:55:07 +0300

    Adding cinemachine + player movements

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float smoothSpeed = 0.1f;
+    Rigidbody2D rb;
+    Animator animator;
+    Vector2 input;
+    Vector2 smoothedInput;
+    Vector2 smoothedInputVelocity;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        smoothedInput = Vector2.SmoothDamp(smoothedInput, input, ref smoothedInputVelocity, smoothSpeed);
+        rb.velocity = smoothedInput * speed;
+    }
+    private void OnMove(InputValue inputValue)
+    {
+        input = inputValue.Get<Vector2>();
+    }
+}
