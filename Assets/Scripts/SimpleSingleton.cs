dae91c85df27bf560ac7cffce51b68f9d2e1927d
reutmgc <reutgaming@gmail.com>
2023-08-21 01:54:39 +0300

    adding google API for communication with google sheets and it's dependencies

--- Assets/Scripts/SimpleSingleton.cs
+++ Assets/Scripts/SimpleSingleton.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SimpleSingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     // thread safety
