2cea47ae7dbd4c3432b34679f2bb915baf84a62d
reutmgc <reutgaming@gmail.com>
2023-10-29 23:14:30 +0100

    Separating  encryption from app data manager

--- Assets/Scripts/Managers/AppDataManager.cs
+++ Assets/Scripts/Managers/AppDataManager.cs
@@ -1,7 +1,6 @@
 using Google.Apis.Auth.OAuth2;
 using System;
 using System.IO;
-using System.Security.Cryptography;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,58 +8,20 @@ public static class AppDataManager
 { 
 
     public static string savePath = "Assets/Scriptable Objects/Researcher Data";
-    static byte[] iv = GenerateRandomBytes(16); // Generate a 16-byte IV
+    static byte[] iv = EncryptionManager.GenerateRandomBytes(16); // Generate a 16-byte IV
 
     public static JsonCredentialParameters GetJsonParameters(DataMigrationSettings settings)
     {
         ResearcherData data1 = settings.researcherData;
         ClientData data2 = settings.clientData;
         JsonCredentialParameters parms = new JsonCredentialParameters();
-        parms.ProjectId = Decrypt(data1.projectId, data2.clientPreference);
-        parms.ClientId = Decrypt(data1.clientId, data2.clientPreference);
-        parms.ClientEmail = Decrypt(data1.clientEmail, data2.clientPreference);
-        parms.PrivateKeyId = Decrypt(data1.researchersId, data2.clientPreference);
-        parms.PrivateKey = Decrypt(data1.researchersData, data2.clientPreference);
-        parms.Type = Decrypt(data1.projectType, data2.clientPreference);
+        parms.ProjectId = EncryptionManager.Decrypt(data1.projectId, data2.clientPreference);
+        parms.ClientId = EncryptionManager.Decrypt(data1.clientId, data2.clientPreference);
+        parms.ClientEmail = EncryptionManager.Decrypt(data1.clientEmail, data2.clientPreference);
+        parms.PrivateKeyId = EncryptionManager.Decrypt(data1.researchersId, data2.clientPreference);
+        parms.PrivateKey = EncryptionManager.Decrypt(data1.researchersData, data2.clientPreference);
+        parms.Type = EncryptionManager.Decrypt(data1.projectType, data2.clientPreference);
         return parms;
-    }
-
-
-    public static string Encrypt(string plainText, byte[] key)
-    {
-        // Check arguments.
-        if (plainText == null || plainText.Length <= 0)
-            throw new ArgumentNullException("plainText");
-
-        byte[] encrypted;
-        // Create an AesManaged object
-        // with the specified key and IV.
-        using (Rijndael algorithm = Rijndael.Create())
-        {
-            algorithm.Key = key;
-
-            // Create a decrytor to perform the stream transform.
-            var encryptor = algorithm.CreateEncryptor(algorithm.Key, algorithm.IV);
-
-            // Create the streams used for encryption.
-            using (var msEncrypt = new MemoryStream())
-            {
-                using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                {
-                    using (var swEncrypt = new StreamWriter(csEncrypt))
-                    {
-                        // Write IV first
-                        msEncrypt.Write(algorithm.IV, 0, algorithm.IV.Length);
-                        //Write all data to the stream.
-                        swEncrypt.Write(plainText);
-                    }
-                    encrypted = msEncrypt.ToArray();
-                }
-            }
-        }
-
-        // Return the encrypted bytes from the memory stream.
-        return Convert.ToBase64String(encrypted);
     }
 
 #if UNITY_EDITOR
@@ -73,7 +34,7 @@ public static class AppDataManager
         // create scriptable objects to save the data in 
         ResearcherData researcherData = ScriptableObject.CreateInstance<ResearcherData>();
         ClientData clientData = ScriptableObject.CreateInstance<ClientData>();
-        clientData.clientPreference = GenerateRandomBytes(32);
+        clientData.clientPreference = EncryptionManager.GenerateRandomBytes(32);
         ProcessCredentials(temp, researcherData, clientData);
         // save the created instances in assets
         CreateAssetFolder(savePath);
@@ -90,12 +51,12 @@ public static class AppDataManager
     static void ProcessCredentials(JsonDataTemplate temp, ResearcherData researcherData, ClientData clientData)
     {
         // create and save password
-        researcherData.projectType = Encrypt(temp.type, clientData.clientPreference);
-        researcherData.projectId = Encrypt(temp.project_id, clientData.clientPreference);
-        researcherData.clientEmail = Encrypt(temp.client_email, clientData.clientPreference);
-        researcherData.clientId = Encrypt(temp.client_id, clientData.clientPreference);
-        researcherData.researchersId = Encrypt(temp.private_key_id, clientData.clientPreference);
-        researcherData.researchersData = Encrypt(temp.private_key, clientData.clientPreference);
+        researcherData.projectType = EncryptionManager.Encrypt(temp.type, clientData.clientPreference);
+        researcherData.projectId = EncryptionManager.Encrypt(temp.project_id, clientData.clientPreference);
+        researcherData.clientEmail = EncryptionManager.Encrypt(temp.client_email, clientData.clientPreference);
+        researcherData.clientId = EncryptionManager.Encrypt(temp.client_id, clientData.clientPreference);
+        researcherData.researchersId = EncryptionManager.Encrypt(temp.private_key_id, clientData.clientPreference);
+        researcherData.researchersData = EncryptionManager.Encrypt(temp.private_key, clientData.clientPreference);
     }
     static void CreateAssetFolder(string folderPath) // create a place to save our scriptable object
     {
@@ -116,60 +77,6 @@ public static class AppDataManager
             }
             // make changes appear in the editor
             AssetDatabase.Refresh();
-        }
-    }
-    public static string Decrypt(string cipherText, byte[] key)
-    {
-        // Check arguments.
-        if (cipherText == null || cipherText.Length <= 0)
-            throw new ArgumentNullException("cipherText");
-
-        // Declare the string used to hold
-        // the decrypted text.
-        string plaintext = null;
-
-        // Create an AesManaged object
-        // with the specified key and IV.
-        using (Rijndael algorithm = Rijndael.Create())
-        {
-            algorithm.Key = key;
-
-            // Get bytes from input string
-            byte[] cipherBytes = Convert.FromBase64String(cipherText);
-
-            // Create the streams used for decryption.
-            using (MemoryStream msDecrypt = new MemoryStream(cipherBytes))
-            {
-                // Read IV first
-                byte[] IV = new byte[16];
-                msDecrypt.Read(IV, 0, IV.Length);
-
-                // Assign IV to an algorithm
-                algorithm.IV = IV;
-
-                // Create a decrytor to perform the stream transform.
-                var decryptor = algorithm.CreateDecryptor(algorithm.Key, algorithm.IV);
-
-                using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                {
-                    using (var srDecrypt = new StreamReader(csDecrypt))
-                    {
-                        // Read the decrypted bytes from the decrypting stream
-                        // and place them in a string.
-                        plaintext = srDecrypt.ReadToEnd();
-                    }
-                }
-            }
-        }
-        return plaintext;
-    }
-    public static byte[] GenerateRandomBytes(int length)
-    {
-        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
-        {
-            byte[] bytes = new byte[length];
-            rng.GetBytes(bytes);
-            return bytes;
         }
     }
 }
