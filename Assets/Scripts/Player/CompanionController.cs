893bfecd6b006df535473e2584a9ebd78b49af95
reutmgc <reutgaming@gmail.com>
2024-04-17 08:04:18 +0200

    astrid follow player + animations

---
+++ Assets/Scripts/Player/CompanionController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionController : MonoBehaviour
+{
+    private bool followPlayer = true;
+    [SerializeField]
+    PlayerMovement companionMovment;
+    public void FollowPlayer()
+    {
+        followPlayer = true;
+        companionMovment.enabled = true;
+    }
+    public void StopFollowingPlayer()
+    {
+        followPlayer = false;
+        companionMovment.enabled = false;
+    }
+}
