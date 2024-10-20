a9a77054a5154973fc6e115417fb51404784e3d8
reutmgc <reutgaming@gmail.com>
2024-10-20 11:53:02 +0300

    Making the simple singleton null if no object exists in the scene instead of dynamically creating one in runtime. This is to avoid cerating new instances from on disable of other classes when the application quits.

--- Assets/Scripts/Managers/SimpleSingleton.cs
+++ Assets/Scripts/Managers/SimpleSingleton.cs
@@ -4,34 +4,29 @@ using System.Collections.Generic;
 using UnityEngine;
 public class SimpleSingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    private static bool isQuitting = false;
 
     private static T instance;
     public static T Instance
     {
         get
         {
-            if (isQuitting)
-                return null;
             if (instance == null)
             {
                 instance = FindObjectOfType<T>();
                 // if couldn't find in scene
-                if (instance == null)
+                /*if (instance == null)
                 {
                     // create new
                     GameObject go = new GameObject(typeof(T).ToString());
                     instance = go.AddComponent<T>();
                     Debug.Log("Instantiating new singleton of type " + typeof(T).ToString());
-                }
+                }*/
             }
             return instance;
         }
     }
     protected virtual void Awake()
-
     {
-        isQuitting = false;
         if (instance == null)
         {
             instance = gameObject.GetComponent<T>();
@@ -45,8 +40,4 @@ public class SimpleSingleton<T> : MonoBehaviour where T : MonoBehaviour
 
     // sometimes, on destroy of one class is called before an on disable of another class. 
     // this is done to avoid a new instance will not be created when the application closes, in the case we request an instance in some other class on disable
-    void OnApplicationQuit()
-    {
-        isQuitting = true;  
-    }
 }
